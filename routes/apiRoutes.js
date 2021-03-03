a2d5bff85098af1c86200e350b602ef747a77ba8
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-03 15:02:01 -0600

    add post push to notes in api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,4 +5,12 @@ module.exports = function(app) {
         res.json(storeData)
     });
 
+    app.post("/api/notes", function (req, res) {
+
+        storeData.push(req.body);
+        res.json(true);
+
+    });
 };
+
+// POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
